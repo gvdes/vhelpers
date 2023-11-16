c98a3100011ad9361839a25968382c63f859037b
hugomers <hugomo0897@gmail.com>
2023-11-16 16:12:29 -0600

    se trabaja con la modificacion de el ticket solo falta los pagos y optimizar un poco ya que hay reactivos que no se ocupan :)

--- src/layouts/ModifyLayout.vue
+++ src/layouts/ModifyLayout.vue
@@ -28,7 +28,7 @@
 
 
       <q-page class="bg-grey-3" padding>
-              <!-- comienza div de opciones hacia el ticket contiene el envio de reimpresion y la devolucion -->
+        <!-- comienza div de opciones hacia el ticket contiene el envio de reimpresion y la devolucion -->
         <div class="row justify-center">
           <transition name="bounce">
             <q-card v-if="ticket.state" style="min-width: 400px;" flat bordered>
@@ -104,6 +104,10 @@
                       <q-item-label caption lines="2">Fecha</q-item-label>
                       <q-item-label class="text-h6">{{ tickmod.body.ticket.fecha }}</q-item-label>
                     </q-item-section>
+                    <q-item-section>
+                      <q-item-label caption lines="2">Total</q-item-label>
+                      <q-item-label class="text-h6">{{ Number.parseFloat(totm).toFixed(2) }}</q-item-label>
+                    </q-item-section>
                   </q-item>
                 </q-card-section>
               </q-card>
@@ -114,9 +118,9 @@
                 <q-card-section>
                   <q-item class="text-h6 ">
                     <q-item-section>
-                      <q-item-label  lines="2">Cliente</q-item-label>
-                      <q-item-label >
-                        <q-form @submit="cambiocliente"  class="q-gutter-md q-display-flex q-justify-center">
+                      <q-item-label lines="2">Cliente</q-item-label>
+                      <q-item-label>
+                        <q-form @submit="cambiocliente" class="q-gutter-md q-display-flex q-justify-center">
                           <q-input v-model="clifac" type="number" style="max-width: 100px;" />
                         </q-form>
                       </q-item-label>
@@ -125,7 +129,21 @@
                   </q-item>
                 </q-card-section>
               </q-card>
+              <q-separator spaced inset vertical dark />
 
+              <q-item>
+                <q-item-section>
+                  <q-card class="my-card">
+                    <q-card-section>
+                      <q-form @submit="searchproduct" class="q-gutter-md">
+                        <div>
+                          <q-input v-model="sarchpro" type="text" label="Agregar Modelo" />
+                        </div>
+                      </q-form>
+                    </q-card-section>
+                  </q-card>
+                </q-item-section>
+              </q-item>
               <q-separator spaced inset vertical dark />
 
               <q-card class="my-card">
@@ -137,14 +155,16 @@
                     <q-item-section>PRECIO</q-item-section>
                     <q-item-section>TOTAL</q-item-section>
                   </q-item>
-                  <q-item v-for="(product, index) in productos" :key="index" @click="seleccionart(product)" clickable
-                    v-ripple>
-                    <q-item-section>{{ product.ARTICULO }}</q-item-section>
-                    <q-item-section>{{ product.DESCRIPCION }}</q-item-section>
-                    <q-item-section>{{ product.CANTIDAD }}</q-item-section>
-                    <q-item-section>{{ product.PRECIO }}</q-item-section>
-                    <q-item-section>{{ product.TOTAL }}</q-item-section>
-                  </q-item>
+                  <q-scroll-area style="height: 300px; max-width: 1700px;">
+                    <q-item v-for="(product, index) in productos" :key="index" @click="seleccionart(product)" clickable
+                      v-ripple>
+                      <q-item-section>{{ product.ARTICULO }}</q-item-section>
+                      <q-item-section>{{ product.DESCRIPCION }}</q-item-section>
+                      <q-item-section >{{ Number.parseFloat(product.CANTIDAD).toFixed(2) }}</q-item-section>
+                      <q-item-section>$ {{ Number.parseFloat(product.PRECIO).toFixed(2) }}</q-item-section>
+                      <q-item-section>$ {{ Number.parseFloat((product.PRECIO * product.CANTIDAD)).toFixed(2) }}</q-item-section>
+                    </q-item>
+                  </q-scroll-area>
                 </q-card-section>
               </q-card>
 
@@ -172,19 +192,7 @@
 
 
 
-              <q-item>
-                <q-item-section>
-                  <q-card class="my-card">
-                    <q-card-section>
-                      <q-form @submit="searchproduct" class="q-gutter-md">
-                        <div>
-                          <q-input v-model="sarchpro" type="text" label="Agregar Modelo" />
-                        </div>
-                      </q-form>
-                    </q-card-section>
-                  </q-card>
-                </q-item-section>
-              </q-item>
+
 
               <q-btn class="full-width" color="primary" icon="receipt" label="PAGAR" @click="cobros" />
 
@@ -193,35 +201,61 @@
                   <q-card-section>
                     <div class="text-h6">Articulo : {{ editprod.body.ARTICULO }}</div>
                     <div class="text-subtitle2">Descripcion : {{ editprod.body.DESCRIPCION }}</div>
-
                   </q-card-section>
                   <q-card-section class="row ">
-                    <q-form>
-
-                      <div class="row justify-between">
-                        <q-input v-model="editprod.body.CANTIDAD" type="number" label="Cantidad"
-                          style="max-width: 50%;" />
-                        <q-input v-model="editprod.body.CANTIDAD" type="number" label="Tarifa"
-                          style="max-width: 50%;" />
-                      </div>
-
-
-                    </q-form>
-
-
+                    <div class="row justify-between">
+                      <q-input v-model="editprod.body.CANTIDAD" type="number" label="Cantidad" style="max-width: 50%;" />
+                    </div>
                   </q-card-section>
                   <q-card-section class="row justify-between">
-                    <div class="text-subtitle2">Precio : {{ editprod.body.PRECIO }}</div>
-                    <div class="text-subtitle2">Total : {{ editprod.body.TOTAL }}</div>
+                    <div class="text-subtitle2">Precio : {{ Number.parseFloat(editprod.body.PRECIO).toFixed(2) }}</div>
+                    <div class="text-subtitle2">Total : {{ Number.parseFloat((editprod.body.PRECIO * editprod.body.CANTIDAD)).toFixed(2) }}</div>
                   </q-card-section>
                   <q-card-actions class="row justify-between">
-                    <q-btn flat icon="delete" color="negative" />
-                    <q-btn flat icon="arrow_forward_ios" :disable="editprod.body.CANTIDAD.length <= 0" v-close-popup />
+                    <q-btn flat icon="delete" color="negative" @click="deleteprod" />
+                    <q-btn flat icon="arrow_forward_ios" :disable="editprod.body.CANTIDAD.length <= 0" @click="edpro" v-close-popup />
                   </q-card-actions>
                 </q-card>
               </q-dialog>
 
+              <q-dialog v-model="addproduct.state" :position="'left'" persistent>
+                <q-card class="my-card">
+                  <q-card-section>
+                    <div class="text-h6">Articulo : {{ addproduct.body.CODART }}</div>
+                    <div class="text-subtitle2">Descripcion : {{ addproduct.body.DESART }}</div>
+                  </q-card-section>
+                  <q-card-section class="row ">
+                    <div class="row justify-between">
+                      <q-input v-model="addproduct.account" type="number" label="Cantidad" style="max-width: 50%;" />
+                    </div>
+                  </q-card-section>
+                  <q-card-section class="row justify-between">
+                    <div class="text-subtitle2">Precio : {{ Number.parseFloat(addproduct.body.PRELTA).toFixed(2) }}</div>
+                    <div class="text-subtitle2">Total : {{ Number.parseFloat((addproduct.body.PRELTA * addproduct.account)).toFixed(2) }}</div>
+                  </q-card-section>
+                  <q-card-actions class="row justify-between">
+                    <q-btn flat icon="close" color="positive" v-close-popup />
+                    <q-btn flat icon="arrow_forward_ios" :disable="addproduct.account <= 0" v-close-popup
+                      @click="addpr" />
+                  </q-card-actions>
+                </q-card>
+              </q-dialog>
 
+              <q-dialog v-model="climod.state">
+                <q-card>
+                  <q-card-section class=" items-center">
+                    <div class="text-h5">Se cambiara el cliente a:</div>
+                    <q-separator spaced inset vertical dark />
+                    <div class="text-h6 text-center text-bold ">{{ climod.body.NOFCLI }}</div>
+                    <q-separator spaced inset vertical dark />
+                    <div class="q-ml-sm text-center">Desea recalcular el ticket ?</div>
+                  </q-card-section>
+                  <q-card-actions align="center">
+                    <q-btn flat icon="close" color="primary" v-close-popup />
+                    <q-btn flat icon="check" color="positive" v-close-popup @click="recalcular" />
+                  </q-card-actions>
+                </q-card>
+              </q-dialog>
             </q-list>
           </q-card>
         </transition>
@@ -266,7 +300,20 @@ const editprod = ref({
   state: false,
   body: null
 })
+const addproduct = ref({
+  state: false,
+  body: null,
+  account: 0
+})
 
+const climod = ref({
+  state: false,
+  body: null
+})
+
+const recl = ref(null)
+
+let totm =  ref(null)
 
 const mod = ref(null);//guarda el movimiento
 const listmod = ["Devolucion", "Modificacion", "Reimpresion"]//listado de movimiento
@@ -311,7 +358,6 @@ const search = async () => {
       });
     });
 }
-
 
 const envia = async () => {
   let host = VDB.session.store.ip;
@@ -389,13 +435,39 @@ const envia = async () => {
         tickmod.value.state = true;
         ticket.value.state = false;
         tickmod.value.body = r.data;
+        totm.value = Number(r.data.ticket.total)
         clifac.value = r.data.ticket.codcli;
         obs1.value = r.data.ticket.observacion;
       })
       .catch(r => r);
   }
 }
-const cambiocliente = () => console.log("secambioelclientebro")
+const cambiocliente = async () => {
+  console.log("cambio de cliente");
+  let client = clifac.value;
+  console.log(client)
+  let host = VDB.session.store.ip;
+  let rux = `http://${host}/access/public/modify/getClient?client=${client}`;
+  axios.get(rux)
+    .then(done => {
+      console.log(done.data)
+      climod.value.body = done.data
+      tickmod.value.body.ticket.cliente = done.data.NOFCLI
+      climod.value.state = true;
+    })
+    .catch(fail => {
+      console.log(fail.response.data);
+      $q.notify({
+        html: true,
+        message: fail.response.data,
+        color: "negative",
+        position: "center"
+      });
+
+    });
+
+
+}
 
 const seleccionart = (a) => {
   console.log(`Mostrando Articulo ${a.ARTICULO}`)
@@ -403,9 +475,105 @@ const seleccionart = (a) => {
   editprod.value.body = a
 
 }
-const searchproduct = () => console.log("buscandoproducto" + sarchpro.value)
+const searchproduct = async () => {
+  console.log("Buscando Producto Y propiedades");
+  let envl = {
+    product: sarchpro.value,
+    price: clifac.value
+  }
+  let bpec = tickmod.value.body.product.findIndex((a) => a.ARTICULO == sarchpro.value)
+  if (bpec == -1) {
+    let host = VDB.session.store.ip;
+    let impr = `http://${host}/access/public/modify/getProduct`;
+    axios.get(impr, { params: envl })
+      .then(done => {
+        console.log(done.data)
+        addproduct.value.body = done.data
+        addproduct.value.state = true
+      })
+      .catch(fail => {
+        console.log(fail.response.data);
+        $q.notify({
+          html: true,
+          message: fail.response.data,
+          color: "negative",
+          position: "center"
+        });
+      });
+
+  } else {
+    $q.notify({
+      html: true,
+      message: `El productos ${sarchpro.value} ya esta en el ticket`,
+      color: "negative",
+      position: "center"
+    });
+
+  }
+
+}
 
 const cobros = () => console.log("Pasa a cobros")
+
+const deleteprod = () => {
+  console.log("Se eliminara el articulo");
+  let art = editprod.value.body.ARTICULO
+  let inx = tickmod.value.body.product.findIndex((a) => a.ARTICULO == art)
+  console.log(inx);
+  tickmod.value.body.product.splice(inx, 1);
+  editprod.value.state = false;
+  totm.value = tickmod.value.body.product.reduce((a,e) =>  a + Number(e.TOTAL),0)
+};
+
+const addpr = () => {
+  let ardpr = {
+    ARTICULO: addproduct.value.body.CODART,
+    DESCRIPCION: addproduct.value.body.DESART,
+    CANTIDAD: addproduct.value.account,
+    PRECIO: addproduct.value.body.PRELTA,
+    TOTAL: addproduct.value.account * addproduct.value.body.PRELTA
+  };
+
+  tickmod.value.body.product.push(ardpr);
+  totm.value = tickmod.value.body.product.reduce((a,e) => a + Number(e.TOTAL),0)
+  addproduct.value.account = 0;
+  sarchpro.value = null
+}
+
+const recalcular = () => {
+  console.log("recalculando ticket");
+  let prmod = tickmod.value.body.product;
+  let clmd = clifac.value
+  let envm = {
+    cliente: clmd,
+    productos: prmod
+  }
+  let host = VDB.session.store.ip;
+  let riwo = `http://${host}/access/public/modify/getPrices`;
+  axios.get(riwo, { params: envm })
+    .then(done => {
+      console.log(done.data)
+      recl.value = done.data
+      recl.value.forEach(e => {
+        let inx = tickmod.value.body.product.findIndex((is) => is.ARTICULO == e.CODART);
+        tickmod.value.body.product[inx].PRECIO = e.PRELTA
+      });
+      totm.value = tickmod.value.body.product.reduce((a,e) => a + (Number(e.CANTIDAD) * Number(e.PRECIO)),0)
+    })
+    .catch(fail => {
+      console.log(fail.response.data);
+      $q.notify({
+        html: true,
+        message: fail.response.data,
+        color: "negative",
+        position: "center"
+      });
+    });
+}
+
+const edpro = () =>{
+  totm.value = tickmod.value.body.product.reduce((a,e) => a + (Number(e.CANTIDAD) * Number(e.PRECIO)),0)
+}
 
 index();
 </script>
