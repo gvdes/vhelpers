f4cb96ab050f1bc7872633be9cf09b8d1c4314de
hugomers <hugomo0897@gmail.com>
2023-12-21 12:08:11 -0600

    se agrega el dominio para brasil 1 ya que presenta problemas la conexion local

---
+++ dist/pwa/assets/use-quasar.242d34a0.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.108b8769.js";function u(){return a(r)}export{u};
--- dist/pwa/assets/use-quasar.c8bb8671.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.de608a9d.js";function u(){return a(r)}export{u};
