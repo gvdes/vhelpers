d04e3efc289b3067ae644cc78bf7d5c90ed1d24e
hugomers <hugomo0897@gmail.com>
2024-02-22 17:47:02 -0600

    se modificarn las ip de ramon corona

---
+++ dist/pwa/assets/use-quasar.0031547c.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.a3c6c41a.js";function u(){return a(r)}export{u};
--- dist/pwa/assets/use-quasar.5f7adf3d.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.190a3bd5.js";function u(){return a(r)}export{u};
