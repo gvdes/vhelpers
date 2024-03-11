bce5c86284a42d008fdb2799d8e52c111a6555b0
hugomers <hugomo0897@gmail.com>
2024-03-11 17:45:47 -0600

    se modifico el axios

--- dist/pwa/assets/use-quasar.752caa70.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.a814028f.js";function u(){return a(r)}export{u};
---
+++ dist/pwa/assets/use-quasar.d4e721f1.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.4a6c1a35.js";function u(){return a(r)}export{u};
