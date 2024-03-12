580e9f6dc354836faca5c3dadf38d0abecc5b93c
hugomers <hugomo0897@gmail.com>
2024-03-12 12:17:49 -0600

    se modificaron usuarios de sucursales

---
+++ dist/pwa/assets/use-quasar.4cbb716f.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.763f4d60.js";function u(){return a(r)}export{u};
--- dist/pwa/assets/use-quasar.d4e721f1.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.4a6c1a35.js";function u(){return a(r)}export{u};
