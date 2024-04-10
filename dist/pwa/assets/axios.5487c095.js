91cb4da2741b56ad6e60c56a0957693d30e3dda6
unknown <jahleelgpv@gmail.com>
2024-04-10 13:47:46 -0600

    se cambia de sucursal a thalia y roberto

--- dist/pwa/assets/axios.5487c095.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./axios.763a3157.js";const t=a.create({baseURL:"http://192.168.10.189/vizapi/public/LVH"}),i=a.create({baseURL:"http://192.168.10.61:1619/Assist/public/api"});export{i as a,t as v};
---
+++ dist/pwa/assets/use-quasar.37357da2.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.880a75cd.js";function u(){return a(r)}export{u};
