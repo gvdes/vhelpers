65d914764bd4e2192475c3e8da919331c5954046
unknown <jahleelgpv@gmail.com>
2024-04-19 11:24:14 -0600

    se cambia usuario de magaly para rc1

--- dist/pwa/assets/use-quasar.51cd8b23.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.84d07889.js";function u(){return a(r)}export{u};
---
+++ dist/pwa/assets/use-quasar.baac3428.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.d0e35c61.js";function u(){return a(r)}export{u};
