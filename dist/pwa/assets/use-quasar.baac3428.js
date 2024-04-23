fb790cb1b466042113e914f9be7c1fcc56249412
unknown <jahleelgpv@gmail.com>
2024-04-23 12:02:35 -0600

    se cambio la ip de apartado 1

--- dist/pwa/assets/use-quasar.baac3428.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.d0e35c61.js";function u(){return a(r)}export{u};
---
+++ dist/pwa/assets/use-quasar.ee4e8b13.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.0a0af4ef.js";function u(){return a(r)}export{u};
