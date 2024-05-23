aedd91b4ee170b76974c0dcb4a13b626116454a0
unknown <jahleelgpv@gmail.com>
2024-05-22 18:20:27 -0600

    se agrega el usuario de angel villasenor en BR1

---
+++ dist/pwa/assets/use-quasar.1a40dee5.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.f167e333.js";function u(){return a(r)}export{u};
--- dist/pwa/assets/use-quasar.5f80f01a.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.6c8d411e.js";function u(){return a(r)}export{u};
