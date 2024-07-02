faf68026b79222a27bd1eeaf6477c82a888ff487
hugomers <hugomo0897@gmail.com>
2024-07-02 15:08:38 -0600

    se agrega usuario richard

---
+++ dist/pwa/assets/use-quasar.145a8b78.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.44dbc2cb.js";function u(){return a(r)}export{u};
--- dist/pwa/assets/use-quasar.776c652b.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.519fe812.js";function u(){return a(r)}export{u};
