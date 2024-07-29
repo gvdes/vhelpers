b6060596b297e0c9a9511d6d02c7baf45420560d
JAHLEEL GARCIA <jahleelgpv@gmail.com>
2024-07-29 15:02:28 -0600

    se cambia el nombre de guillermo por jose adrian martinez

--- dist/pwa/assets/axios.f7219b88.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./axios.39a83f93.js";const t=a.create({baseURL:"http://192.168.10.189/vizapi/public/LVH"}),i=a.create({baseURL:"http://192.168.10.61:1619/Assist/public/api"});export{i as a,t as v};
--- dist/pwa/assets/use-quasar.5ebfddba.js
+++
@@ -1,1 +0,0 @@
-import{p as a,$ as r}from"./index.135f19df.js";function u(){return a(r)}export{u};
