73c0c99614b83411996c2b8e8168a7a78e915a1a
HUGO MUÑOZ <hugomo0897@gmail.com>
2024-08-20 12:58:37 -0600

    se agrega funcion en ventas

---
+++ dist/pwa/assets/use-quasar.a0b9779e.js
@@ -0,0 +1,1 @@
+import{p as a,$ as r}from"./index.eea6daba.js";function u(){return a(r)}export{u};
--- src/layouts/SaleStore.vue
+++ src/layouts/SaleStore.vue
@@ -27,17 +27,7 @@
           <q-card-section>
             <div class="text-h6 text-center">Ventas 2023</div>
             <div class="text-h4 text-center">
-              {{
-                Number(
-                  stores.reduce((accumulator, store) => {
-                    return (
-                      accumulator + Number(store.sales?.salesant * 1.1 || 0)
-                    );
-                  }, 0)
-                )
-                  .toFixed(2)
-                  .replace(/\d(?=(\d{3})+\.)/g, "$&,")
-              }}
+              {{ ventAnt.replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
             </div>
           </q-card-section>
           <q-card-section>
@@ -62,15 +52,7 @@
           <q-card-section>
             <div class="text-h6 text-center">Ventas 2024</div>
             <div class="text-h4 text-center">
-              {{
-                Number(
-                  stores.reduce((accumulator, store) => {
-                    return accumulator + Number(store.sales?.salesact || 0);
-                  }, 0)
-                )
-                  .toFixed(2)
-                  .replace(/\d(?=(\d{3})+\.)/g, "$&,")
-              }}
+              {{ ventAct.replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
             </div>
           </q-card-section>
           <q-card-section>
@@ -95,20 +77,7 @@
           <q-card-section>
             <div class="text-h6 text-center">Diferencia</div>
             <div class="text-h4 text-center">
-              {{
-                Number(
-                  stores.reduce((accumulator, store) => {
-                    return (
-                      accumulator +
-                      Number(
-                        store.sales?.salesact - store.sales?.salesant * 1.1 || 0
-                      )
-                    );
-                  }, 0)
-                )
-                  .toFixed(2)
-                  .replace(/\d(?=(\d{3})+\.)/g, "$&,")
-              }}
+              {{ Diferencia.replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
             </div>
           </q-card-section>
           <q-card-section>
@@ -138,10 +107,7 @@
           <q-card-section>
             <div class="text-h6 text-center">VENTAS HOY</div>
             <div class="text-h4 text-center">
-              {{
-                Number(stores.reduce((accumulator, store) => { return accumulator + Number(store.sales?.saleshoy || 0); },
-                  0)).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")
-              }}
+              {{ VentasHoy.replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
             </div>
           </q-card-section>
           <q-card-section>
@@ -159,8 +125,8 @@
 
       <q-separator spaced inset vertical dark />
 
-      <q-table title="Sucursales" :rows="stores" row-key="id" :pagination="table.pagination" :filter="table.filter"
-        hide-header>
+      <q-table title="Sucursales" :rows="stores.sort((a, b) => b.sales?.saleshoy - a.sales?.saleshoy)" row-key="id"
+        :pagination="table.pagination" :filter="table.filter" hide-header>
         <template v-slot:top-right>
           <q-input borderless dense debounce="300" v-model="table.filter" placeholder="Buscar">
             <template v-slot:append>
@@ -174,41 +140,31 @@
             <q-list bordered dense>
               <q-item>
                 <q-item-section>
-
-                  <q-item-label overline>{{ props.row.name }}</q-item-label>
-                  <q-item-label caption>{{ props.row.alias }}</q-item-label>
-                  <div></div>
-
+                  <q-item-label class="flex justify-between">
+                    <div class="text-bold">{{ props.row.name }}</div>
+                    <div class="text-overline">{{ `Tickets ${props.row.sales?.hoytck}` }}
+                      <q-btn color="grey" flat dense :icon="props.expand ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
+                        " @click="props.expand = !props.expand" />
+                    </div>
+                  </q-item-label>
+                  <q-item-label>
+                    <div class="q-pa-sm flex justify-start">
+                      <q-linear-progress :value="Number(props.row.sales?.saleshoy) / Number(VentasHoy)" :buffer="1"
+                        size="20px">
+                        <div class="absolute-full flex flex-center">
+                          <q-badge color="white" text-color="accent"
+                            :label="`$ ${Number(props.row.sales?.saleshoy).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`" />
+                        </div>
+                      </q-linear-progress>
+                    </div>
+                  </q-item-label>
                 </q-item-section>
 
-                <q-item-section class="text-right" v-if="!props.expand">
-                  <div v-if="props.row.sales">
-                    <q-item-label overline>$
-                      {{
-                        Number(props.row.sales?.saleshoy)
-                          .toFixed(2)
-                          .replace(/\d(?=(\d{3})+\.)/g, "$&,")
-                      }}
-                    </q-item-label>
-                    <q-item-label caption>tickets: {{ props.row.sales?.hoytck }}</q-item-label>
-                  </div>
-                  <div v-else>
-                    <q-item-label caption> <q-circular-progress indeterminate rounded size="20px" color="primary"
-                        class="q-ma-md" /></q-item-label>
-                  </div>
-
-
-
-                </q-item-section>
-                <q-item-section avatar v-if="!ismobile">
-                  <q-btn color="grey" flat dense :icon="props.expand ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
-                    " @click="props.expand = !props.expand" />
-                </q-item-section>
               </q-item>
               <q-slide-transition>
                 <div class="" v-show="props.expand" v-if="props.row.sales">
                   <q-separator />
-                  <q-item class="text-center" dense>
+                  <q-item class="text-center" dense clickable v-ripple @click="mostInf(props.row)">
                     <q-item-section>
                       <q-item-label overline>VENTAS 2023</q-item-label>
                       <q-item-label caption>{{
@@ -222,7 +178,7 @@
                         props.row.sales.tiketsant
                       }}</q-item-label>
                     </q-item-section>
-                    
+
                     <q-item-section>
                       <q-item-label overline>Promedio 2023</q-item-label>
                       <q-item-label caption>{{
@@ -294,15 +250,150 @@
         </template>
       </q-table>
 
+      <q-dialog v-model="viewSaleSuc.state"  full-width>
+        <q-card class="my-card">
+          <q-card-section>
+            <div class="text-h6">{{ viewSaleSuc.val.name }}</div>
+            <div class="text-subtitle2">{{ viewSaleSuc.val.alias }}</div>
+          </q-card-section>
+          <q-card-section class="row">
+
+            <q-card class="my-card col">
+              <q-card-section>
+                <div class="text-h6 text-center">Ventas 2023</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.salesant).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6 text-center">Tickets 2023</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.tiketsant).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6 text-center">Tickets Promedio 2023</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.salesant / viewSaleSuc.val.sales.tiketsant).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+            </q-card>
+
+            <q-separator spaced inset vertical dark />
+
+            <q-card class="my-card col">
+              <q-card-section>
+                <div class="text-h6 text-center">Ventas 2024</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.salesact).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6 text-center">Tickets 2024</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.tiketsact).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6 text-center">Tickets Promedio 2024</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.salesact / viewSaleSuc.val.sales.tiketsact).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+            </q-card>
+
+            <q-separator spaced inset vertical dark />
+
+            <q-card class="my-card col">
+              <q-card-section>
+                <div class="text-h6 text-center">Diferencias</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.salesact - viewSaleSuc.val.sales.salesant).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6 text-center">Diferencia Tickts</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.tiketsact - viewSaleSuc.val.sales.tiketsant).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+            </q-card>
+
+            <q-separator spaced inset vertical dark />
+
+            <q-card class="my-card col">
+              <q-card-section>
+                <div class="text-h6 text-center">Ventas Hoy</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.saleshoy).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6 text-center">Tickets Hoy</div>
+                <div class="text-h5 text-center">
+                  {{ Number(viewSaleSuc.val.sales.hoytck).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}
+                </div>
+              </q-card-section>
+            </q-card>
+          </q-card-section>
+          <q-card-section>
+            <q-card class="my-card">
+              <q-card-section>
+                <q-list bordered>
+                  <q-item>
+                    <q-item-section avatar>
+                      <!-- <q-icon color="primary" name="bluetooth" /> -->
+                    </q-item-section>
+                    <q-item-section>Caja</q-item-section>
+                    <q-item-section>Venta</q-item-section>
+                    <q-item-section>Tickets</q-item-section>
+                    <q-item-section>Promedio</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6">Resumen Dia</div>
+                <q-list bordered>
+                  <q-item v-for="(cajas,index) in viewSaleSuc.val.sales.ventasdepday" :key="index">
+                    <q-item-section avatar>
+                      <q-icon color="primary" name="point_of_sale" />
+                    </q-item-section>
+                    <q-item-section>{{ cajas.DESTER }}</q-item-section>
+                    <q-item-section>{{ Number(cajas.VENTA).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}</q-item-section>
+                    <q-item-section>{{ cajas.TCK }}</q-item-section>
+                    <q-item-section>{{ Number(cajas.VENTA/ cajas.TCK).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6">Resumen Mensual</div>
+                <q-list bordered>
+                  <q-item v-for="(cajas,index) in viewSaleSuc.val.sales.ventasdepmonth" :key="index" >
+                    <q-item-section avatar>
+                      <q-icon color="primary" name="point_of_sale" />
+                    </q-item-section>
+                    <q-item-section>{{ cajas.DESTER }}</q-item-section>
+                    <q-item-section>{{ Number(cajas.VENTA).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}</q-item-section>
+                    <q-item-section>{{ cajas.TCK }}</q-item-section>
+                    <q-item-section>{{ Number(cajas.VENTA/ cajas.TCK).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") }}</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-card-section>
+            </q-card>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
+
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup>
-import { ref, computed, watch } from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { ref, computed, watch, onMounted } from "vue";
+import { loadRouteLocation, useRoute, useRouter } from "vue-router";
 import { AppFullscreen, useQuasar } from "quasar";
+import Chart from 'chart.js/auto';
 import dayjs from "dayjs";
 import UserToolbar from "src/components/UserToolbar.vue";
 import ApiAssist from "src/API/assistApi";
@@ -314,11 +405,17 @@ const $router = useRouter();
 const $user = useVDBStore();
 
 
+
 const stores = ref([]);
 const table = ref({
   pagination: { rowsPerPage: 0 },
   filter: "",
 });
+
+const viewSaleSuc = ref({
+  state: false,
+  val: null,
+})
 
 const smonth = ref({
   val: null
@@ -340,8 +437,25 @@ const meses = [
 ]
 
 
-
 const ismobile = computed(() => $q.platform.is.mobile);
+const ventAnt = computed(() => {
+  return Number(stores.value.reduce((accumulator, store) => { return (accumulator + Number(store.sales?.salesant * 1.1 || 0)); }, 0)).toFixed(2)
+})
+const ventAct = computed(() => {
+  return Number(stores.value.reduce((accumulator, store) => { return accumulator + Number(store.sales?.salesact || 0); }, 0)).toFixed(2)
+})
+
+const Diferencia = computed(() => {
+  return Number(stores.value.reduce((accumulator, store) => { return (accumulator + Number(store.sales?.salesact - store.sales?.salesant * 1.1 || 0)); }, 0)).toFixed(2)
+})
+
+const VentasHoy = computed(() => {
+  return Number(stores.value.reduce((accumulator, store) => { return accumulator + Number(store.sales?.saleshoy || 0); }, 0)).toFixed(2)
+})
+
+
+
+
 
 const init = async () => {
   const date = new Date();
@@ -356,7 +470,6 @@ const init = async () => {
     console.log(resp);
     stores.value = resp;
     getSale(stores.value, mes);
-
   }
 };
 
@@ -365,7 +478,9 @@ const getSale = async (sucursales, mes) => {
   sucursales.forEach((e, index) => {
     setTimeout(() => {
       let host = e.ip_address;
-      let sale = `http://${host}/access/public/reports/getSalesPerMonth/${mes}`;
+      // let sale = `http://${host}/access/public/reports/getSalesPerMonth/${mes}`;
+      let sale = `http://192.168.10.160:1619/access/public/reports/getSalesPerMonth/${mes}`;
+
       axios
         .get(sale)
         .then((done) => {
@@ -380,7 +495,6 @@ const getSale = async (sucursales, mes) => {
   $q.loading.hide();
 };
 
-
 if ($user.session.rol == 'root') {
   init()
 } else {
@@ -391,6 +505,12 @@ if ($user.session.rol == 'root') {
 const ObtReport = () => {
   stores.value.map(e => e.sales = null);
   getSale(stores.value, smonth.value.val.id)
+}
+
+const mostInf = (rows) => {
+  viewSaleSuc.value.state = true
+  viewSaleSuc.value.val = rows
+  console.log(rows)
 }
 
 </script>
