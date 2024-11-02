e9524bb395809537aaf7a8abfaa94af3ac47a541
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2024-11-02 13:19:32 -0600

    se agrego el usuario de gerente de hugo martinez

---
+++ dist/pwa/assets/Transfers.60fb9ae8.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.6902f034.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.0507a54d.js";import"./QResizeObserver.fc5beeb0.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
--- dist/pwa/assets/Transfers.cad68ec2.js
+++
@@ -1,1 +0,0 @@
-import{Q as t,a as r}from"./QLayout.8bb11fc5.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.09c886d2.js";import"./QResizeObserver.c42d9972.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
