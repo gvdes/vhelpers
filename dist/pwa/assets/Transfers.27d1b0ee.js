d9486b892d74db29137615d14e7f05ffac18f892
HUGO MUÑOZ <hugomo0897@gmail.com>
2024-11-15 21:09:52 -0600

    :se agrega nueva ip

---
+++ dist/pwa/assets/Transfers.27d1b0ee.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.981f5ab2.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.11fd406c.js";import"./QResizeObserver.ff3cc7a3.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
