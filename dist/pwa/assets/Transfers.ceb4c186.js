a3e8f904a6d8ff8b81cbc68aa63eeae74dc11277
HUGO MUÑOZ <hugomo0897@gmail.com>
2024-11-15 21:04:17 -0600

    se modico ip

--- dist/pwa/assets/Transfers.ceb4c186.js
+++
@@ -1,1 +0,0 @@
-import{Q as t,a as r}from"./QLayout.29dc16c3.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.b119c6a8.js";import"./QResizeObserver.77e4acce.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
