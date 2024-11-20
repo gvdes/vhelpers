1178d229c0e03cac771df2e28589f1ae54a94542
HUGO MUÑOZ <hugomo0897@gmail.com>
2024-11-19 18:54:33 -0600

    se agregan usuarios

--- dist/pwa/assets/QSpace.c1360956.js
+++
@@ -1,1 +0,0 @@
-import{k as a,j as e}from"./index.ef22d98d.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
--- dist/pwa/assets/Transfers.5f514a7d.js
+++
@@ -1,1 +0,0 @@
-import{Q as t,a as r}from"./QLayout.4cd09559.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.ef22d98d.js";import"./QResizeObserver.279be3cd.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
