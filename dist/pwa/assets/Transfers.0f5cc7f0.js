46b8300df60c1fc3912ae091280203cb845539b1
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2024-11-26 10:46:01 -0600

    se combia la ip de sotano

--- dist/pwa/assets/Transfers.0f5cc7f0.js
+++
@@ -1,1 +0,0 @@
-import{Q as t,a as r}from"./QLayout.fa1b4bdd.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.96e68add.js";import"./QResizeObserver.d759c7de.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
