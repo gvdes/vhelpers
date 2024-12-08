02c5680c500f6cebf6db413b916893a098998602
HUGO MUÑOZ <hugomo0897@gmail.com>
2024-12-08 10:24:28 -0600

    se cambia ip de vrasil

---
+++ dist/pwa/assets/QSpace.c4381b3c.js
@@ -0,0 +1,1 @@
+import{k as a,j as e}from"./index.f73eb6be.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ dist/pwa/assets/Transfers.3e4f3671.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.110f5e14.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.f73eb6be.js";import"./QResizeObserver.1fbfaef9.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
