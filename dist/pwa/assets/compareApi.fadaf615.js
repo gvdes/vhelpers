6e596c6dd9a63bb72d5f5dee4a4e5428a5734c8e
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-01-16 12:04:12 -0600

    se agrega dist

---
+++ dist/pwa/assets/compareApi.fadaf615.js
@@ -0,0 +1,1 @@
+import{v as e}from"./axios.ae0a941c.js";var o={getData(t,a){return e.post(`/compare/getProducts/${t}`,a).then(r=>r.data).catch(r=>({fail:r}))},secciones(){return e.get("/compare").then(t=>t.data).catch(t=>({fail:t}))},getProductReport(){return e.get("/compare/getProducts").then(t=>t.data).catch(t=>({fail:t}))}};export{o as d};
