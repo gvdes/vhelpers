241a86f51c2e0bf0faaa4282947bd0d282411339
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2025-01-30 10:04:43 -0600

    se cambia de sucursal maurilio

--- dist/pwa/assets/QCardActions.5f840636.js
+++
@@ -1,1 +0,0 @@
-import{j as r,ar as t,as as c,c as l,k as n,l as i}from"./index.9f103d50.js";var d=r({name:"QCardActions",props:{...t,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=l(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>n("div",{class:o.value},i(s.default))}});export{d as Q};
--- dist/pwa/assets/Transfers.feb921bc.js
+++
@@ -1,1 +0,0 @@
-import{Q as t,a as r}from"./QLayout.5ec76129.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.9f103d50.js";import"./QResizeObserver.f75f0dbb.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
