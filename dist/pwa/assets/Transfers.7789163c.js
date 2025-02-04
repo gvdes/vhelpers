fbac885ee365535e946bcd641702bf9f63e404dc
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2025-02-04 12:13:50 -0600

    se modifican cambios de sucursal de compañero (luis fernando , alejandra loza y gloria perez)

---
+++ dist/pwa/assets/Transfers.7789163c.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.60782979.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.e3f6c2b2.js";import"./QResizeObserver.a1b779a8.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
