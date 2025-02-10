523e45efd924ab1587dc1d00049a1a549632f8e4
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2025-02-10 14:10:14 -0600

    se agrega el usuario de luis angel de ecommerce con permisos de (caj)

--- dist/pwa/assets/QCardActions.f92932b0.js
+++
@@ -1,1 +0,0 @@
-import{j as r,ar as t,as as c,c as l,k as n,l as i}from"./index.e3f6c2b2.js";var d=r({name:"QCardActions",props:{...t,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=l(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>n("div",{class:o.value},i(s.default))}});export{d as Q};
--- dist/pwa/assets/QSpace.e57c77af.js
+++
@@ -1,1 +0,0 @@
-import{k as a,j as e}from"./index.e3f6c2b2.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
