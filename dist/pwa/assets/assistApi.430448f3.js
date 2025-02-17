8d8dbd9bc021684e9e290441c1a5ef9857e4779c
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-02-17 09:39:30 -0600

    se modifica id de israel

--- dist/pwa/assets/assistApi.430448f3.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./axios.30672b3a.js";var r={index(){return e.get("/sales/getSale").then(t=>t.data).catch(t=>({fail:t}))},getReport(){return e.get("/getResourses").then(t=>t.data).catch(t=>({fail:t}))}};export{r as a};
