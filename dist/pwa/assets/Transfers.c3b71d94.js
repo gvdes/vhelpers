cc7717b20b0d9b49c0471e45fcc68b90c495c224
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2025-02-27 17:52:03 -0600

    se realizan cambios de sucursales a luis reyes, luis fernando y alejandra loza

---
+++ dist/pwa/assets/Transfers.c3b71d94.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.b7211c56.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.6553c4a3.js";import"./QResizeObserver.718ce7c7.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
