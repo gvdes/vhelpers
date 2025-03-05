fab94502c3b72077475e9fc9ab177d25ef72a7a1
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-03-05 15:03:33 -0600

    se modifica el acceso de gloria

---
+++ dist/pwa/assets/Transfers.0bc91e18.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.c9bb255d.js";import{aW as s,o as n,a as _,w as e,e as a}from"./index.799ea64d.js";import"./QResizeObserver.d6ed544e.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=s("router-view");return n(),_(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
