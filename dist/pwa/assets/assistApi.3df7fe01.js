7daebae70c8ce6f2f1601827ac1ea4d1e354acea
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2025-03-12 10:47:51 -0600

    se dan de alta usuarios de miri,diego y chikis

---
+++ dist/pwa/assets/assistApi.3df7fe01.js
@@ -0,0 +1,1 @@
+import{a as e}from"./axios.30672b3a.js";var a={index(){return e.get("/sales/getSale").then(t=>t.data).catch(t=>({fail:t}))},getReport(){return e.get("/getResourses").then(t=>t.data).catch(t=>({fail:t}))}};export{a as A};
--- dist/pwa/assets/compareApi.0293d6bf.js
+++
@@ -1,1 +0,0 @@
-import{v as e}from"./axios.ba7b1b0e.js";var o={getData(t,a){return e.post(`/compare/getProducts/${t}`,a).then(r=>r.data).catch(r=>({fail:r}))},secciones(){return e.get("/compare").then(t=>t.data).catch(t=>({fail:t}))},getProductReport(){return e.get("/compare/getProducts").then(t=>t.data).catch(t=>({fail:t}))}};export{o as d};
