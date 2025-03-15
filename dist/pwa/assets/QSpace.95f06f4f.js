c660ddc7ecc9c2fba940da383ec34baa531352d7
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-03-14 18:52:28 -0600

    se agrega el dist

---
+++ dist/pwa/assets/QSpace.95f06f4f.js
@@ -0,0 +1,1 @@
+import{i as e,j as s}from"./index.ad93775a.js";var c=e({name:"QSpace",setup(){const a=s("div",{class:"q-space"});return()=>a}});export{c as Q};
---
+++ dist/pwa/assets/Transfers.291fc025.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.b8466695.js";import{aW as s,o as n,a as _,w as e,e as a}from"./index.ad93775a.js";import"./QResizeObserver.cf4e2aed.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=s("router-view");return n(),_(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
