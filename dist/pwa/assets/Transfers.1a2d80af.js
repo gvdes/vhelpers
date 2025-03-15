f9fa767ed4042b8f76596c3f99594857a16b5e02
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-03-14 18:55:46 -0600

    se pone dist

---
+++ dist/pwa/assets/Transfers.1a2d80af.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.5be6e713.js";import{aW as s,o as n,a as _,w as e,e as a}from"./index.ee411de5.js";import"./QResizeObserver.3a322cf2.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=s("router-view");return n(),_(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
