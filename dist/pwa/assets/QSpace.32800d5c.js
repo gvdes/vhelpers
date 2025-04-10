b6acc4a3c0de676b6cb383a7c8bc459df33e6dc1
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2025-04-10 11:40:11 -0600

    se agregan usuario de alexis en ra2 y ra1

---
+++ dist/pwa/assets/QSpace.32800d5c.js
@@ -0,0 +1,1 @@
+import{k as a,j as e}from"./index.7a652f76.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
--- dist/pwa/assets/Transfers.11ead703.js
+++
@@ -1,1 +0,0 @@
-import{Q as t,a as r}from"./QLayout.43dfd575.js";import{aW as s,o as n,a as _,w as e,e as a}from"./index.cbc14e05.js";import"./QResizeObserver.cbf35e3d.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=s("router-view");return n(),_(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
