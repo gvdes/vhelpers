175311a874ca25d166f5785bd17c46cf5460550e
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-04-28 12:32:15 -0600

    se modifica

---
+++ dist/pwa/assets/Transfers.36360861.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.e8e3128b.js";import{aW as s,o as n,a as _,w as e,e as a}from"./index.8d4418ba.js";import"./QResizeObserver.9289ca1a.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=s("router-view");return n(),_(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
