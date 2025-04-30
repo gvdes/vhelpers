5f1e7837416c9fb9641535f6cca06660e4da1f23
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2025-04-30 17:23:59 -0600

    se modifican id's de usuarios

---
+++ dist/pwa/assets/Transfers.cd3eea6d.js
@@ -0,0 +1,1 @@
+import{Q as t,a as r}from"./QLayout.668166fa.js";import{o as s,a as n,w as e,aV as _,e as a}from"./index.8ab51c4d.js";import"./QResizeObserver.8f0b3f5c.js";const l={__name:"Transfers",setup(c){return(p,u)=>{const o=_("router-view");return s(),n(t,{view:"hHh Lpr fFf"},{default:e(()=>[a(r,null,{default:e(()=>[a(o)]),_:1})]),_:1})}}};export{l as default};
