358cef739643ae382c741a10f42f66509a02d0fe
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-05-05 18:40:24 -0600

    se agrega dist

--- dist/pwa/assets/assistApi.3dfee3df.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./axios.6d2993c0.js";var a={index(){return e.get("/sales/getSale").then(t=>t.data).catch(t=>({fail:t}))},getReport(){return e.get("/getResourses").then(t=>t.data).catch(t=>({fail:t}))}};export{a as A};
