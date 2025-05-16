33d736e2da17ded1d99926120cb73816387f352d
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-05-16 16:22:35 -0600

    se agrega modulo de reportes en sucursal

---
+++ dist/pwa/assets/QSpace.6bf375f5.js
@@ -0,0 +1,1 @@
+import{i as e,j as s}from"./index.d07968d6.js";var c=e({name:"QSpace",setup(){const a=s("div",{class:"q-space"});return()=>a}});export{c as Q};
--- dist/pwa/assets/QSpace.7270fdbe.js
+++
@@ -1,1 +0,0 @@
-import{k as a,j as e}from"./index.d494b13b.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
