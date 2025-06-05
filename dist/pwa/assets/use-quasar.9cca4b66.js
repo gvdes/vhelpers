ee268fc686afac69525f8f1985ad875158ac75ef
JAHLEELGARCIA <166541906+JAHLEELGARCIA@users.noreply.github.com>
2025-06-05 15:09:55 -0600

    se agrego usuario de agustin asi mismo se agrego modulo ref para permiso de devoluciones de mercancia

---
+++ dist/pwa/assets/use-quasar.9cca4b66.js
@@ -0,0 +1,1 @@
+import{F as a,U as r}from"./index.d5f50973.js";function u(){return a(r)}export{u};
--- dist/pwa/assets/use-quasar.cf091104.js
+++
@@ -1,1 +0,0 @@
-import{F as a,U as r}from"./index.6b919260.js";function u(){return a(r)}export{u};
