0c23f509dc767602d410ff251acf49c2e28372c0
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-07-14 16:49:45 -0600

    se modifica la factura

--- dist/pwa/assets/assistApi.85dbe223.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./axios.48019e0f.js";var s={index(){return e.get("/sales/getSale").then(t=>t.data).catch(t=>({fail:t}))},getReport(){return e.get("/getResourses").then(t=>t.data).catch(t=>({fail:t}))}};export{s as A};
