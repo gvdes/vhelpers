0203cca80312499d9fcaeed5e476d5514fe58041
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-07-15 10:41:57 -0600

    se modifica para que puedan hacer rtraspasos

--- dist/pwa/assets/auth.e9466448.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./axios.bfe39b1e.js";var a={trySignin(r){return e.post("/users/trySignin",r).then(t=>t.data).catch(t=>({fail:t}))},getResources(r){return e.get(`/users/getResources/${r}`).then(t=>t.data).catch(t=>({fail:t}))}};export{a};
