88e96d97c4c7e3beb63e231e35cc4ad508304dda
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-07-15 13:37:30 -0600

    se modifica socket

---
+++ dist/pwa/assets/viewRefund.c666d57b.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.80461780.js";import{b0 as o,o as r,d as a}from"./index.c896f958.js";const t={};function s(c,n,p,d,f,_){return r(),a(e,{padding:""})}var u=o(t,[["render",s]]);export{u as default};
