b1769ccf11253d74b51a4c013e7116dca2515fd1
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-07-15 11:05:00 -0600

    se agregan almacenes a validadores

--- dist/pwa/assets/viewRefund.56dcc227.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.09402950.js";import{b0 as o,o as r,d as a}from"./index.0fe4016f.js";const t={};function s(c,n,p,d,f,_){return r(),a(e,{padding:""})}var u=o(t,[["render",s]]);export{u as default};
