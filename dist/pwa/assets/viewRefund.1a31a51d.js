a37eb622f3381c0c2934999411699378747ceaa8
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-07-15 17:40:08 -0600

    se modifica la parte de verificacion

--- dist/pwa/assets/viewRefund.1a31a51d.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.5692c218.js";import{b0 as o,o as r,d as a}from"./index.04fed6f3.js";const t={};function s(c,n,p,d,f,_){return r(),a(e,{padding:""})}var u=o(t,[["render",s]]);export{u as default};
