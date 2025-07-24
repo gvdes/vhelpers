e2ad0fcf314107803990effefd2eb561935c5f41
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-07-23 19:32:53 -0600

    se modifica el de resurtido y0yyyyyy el de checkout

--- dist/pwa/assets/layoutStore.7efc8635.js
+++
@@ -1,1 +0,0 @@
-import{b5 as e}from"./index.57501730.js";const o=e("layout",{state:()=>({title:"Pedidos Insumos",showCreateButton:!0,showOrders:!0}),actions:{setTitle(t){this.title=t},setShowCreateButton(t){this.showCreateButton=t},setShowOrders(t){this.showOrders=t}}});export{o as u};
