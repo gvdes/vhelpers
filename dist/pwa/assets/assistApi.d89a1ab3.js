a1a587e767d4e50577a6779cf3e4cd5f9875cbbe
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-07-25 17:22:13 -0600

    se agrega el docena

---
+++ dist/pwa/assets/assistApi.d89a1ab3.js
@@ -0,0 +1,1 @@
+import{k as a}from"./axios.1aa5578e.js";var s={index(){return a.get("/sales/getSale").then(t=>t.data).catch(t=>({fail:t}))},getReport(){return a.get("/getResourses").then(t=>t.data).catch(t=>({fail:t}))},getSales(t){return a.post("/sales/getSale",t).then(e=>e.data).catch(e=>({fail:e}))}};export{s as A};
--- dist/pwa/assets/viewRefund.3a3919df.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.dcdce54d.js";import{b3 as o,o as r,d as a}from"./index.ca87d28d.js";const t={};function s(c,n,p,d,f,_){return r(),a(e,{padding:""})}var u=o(t,[["render",s]]);export{u as default};
