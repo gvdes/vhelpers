6e2af6215ce9f174b500191c427a95ee3fb057d2
suli <duplicaron@gamil.com>
2026-02-13 12:14:10 -0600

    se modifica los alias de quien puede generar salida internas

--- src/pages/Outputs/viewOutputs.vue
+++ src/pages/Outputs/viewOutputs.vue
@@ -198,7 +198,7 @@ const init = async () => {
   } else {
     console.log(resp)
     $q.loading.hide();
-    if (VDB.session.rol == 'aud' || VDB.session.rol == 'audc' || VDB.session.rol == 'root' || VDB.session.rol == 'des') {
+    if (VDB.session.rol == 'gro' || VDB.session.rol == 'jfz' || VDB.session.rol == 'root' || VDB.session.rol == 'des'  || VDB.session.rol == 'axo') {
       salida.value = resp
       products.value = resp.bodie
       console.log(salida.value)
